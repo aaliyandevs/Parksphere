da84f61765db80fccf10da7be5ec6e446a2b9402
M. Aaliyan <aaliyan.devs@gmail.com>
2025-11-28 19:23:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle")
+const navbarMobile = document.getElementById("navbarMobile")
+
+toggle.addEventListener("click", function(){
+    navbarMobile.classList.toggle("hide")
+})
